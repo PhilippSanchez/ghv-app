8ff4fee6776661ab9f489bc7e78037fceb70c631
Philipp Sanchez Paetzmann <containerkid@gmail.com>
2022-12-30 18:47:48 +0100

    Radical Changes to the Code

--- src/script.js
+++ src/script.js
@@ -1,9 +1,31 @@
-let auftragsCounter = 0;
 
 
+const testFunc = () => console.log('Test Running');
 
-let neuerAuftrag = document.createElement('tr');
-neuerAuftrag.innerHTML =  `<td class="objNr.">${auftragsCounter}<td><td>601-TFK<td>`;
+let counter = 1;
+
+function neuerAuftrag() {
+    
 
 
-document.getElementsByClassName('usertable-body').appendChild(neuerAuftrag);
+    let newCounterTd = `<td>${counter}</td>`;
+    let newObjectTd = `<td>${prompt('Gib die Object Nummer ein')}</td>`;
+    let issueTd = `<td>${prompt('Informationen zum Problem')}</td>`;
+    let zuweisungTd = `<td><img src="/Users/rimlight/Documents/GHV PVT APP/ghv-app/library/Memojis/IMG_4655.PNG" style="height: 30px;"></td>`;
+
+
+    let newRow = document.createElement('tr');
+    newRow.classList.add('auftrags-row');
+    newRow.innerHTML = newCounterTd + newObjectTd + issueTd + zuweisungTd;
+    
+    document.querySelector('.usertable-body').appendChild(newRow)
+    console.log(newRow)
+
+    counter++;
+
+
+}
+
+function deleteRow(){
+    
+}
