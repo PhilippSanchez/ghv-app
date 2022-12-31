4851643df0e8c875cdbef6b5c6a9bde58f962bd2
Gonzo <containerkid@gmail.com>
2022-12-31 11:40:57 +0100

    Changed the Modal and the Modal functionality

--- src/script.js
+++ src/script.js
@@ -3,16 +3,16 @@ let counter = 1;
 
 function neuerAuftrag() {
     
-
-
+    let auftragsTitle = document.querySelector('#auftrags-title').innerText;
+    console.log(auftragsTitle);
     let newCounterTd = `<td>${counter}</td>`;
     let newObjectTd = `<td>${prompt('Gib die Object Nummer ein')}</td>`;
-    let issueTd = `<td>${prompt('Informationen zum Problem')}</td>`;
+    let issueTd = `<td>${auftragsTitle}</td>`;
     let zuweisungTd = `<td><img src="/Users/rimlight/Documents/GHV PVT APP/ghv-app/library/Memojis/IMG_4655.PNG" style="height: 30px;"></td><td>…</td><td>…</td>`;
     let removeBtn = '<td onclick="deleteRow()" class="rowBtn deleteRowBtn">…</td>'
     let inspectBtn = '<td onclick="inspectRow()" class="rowBtn inspectRowBtn">…</td>'
     let prioTd = '<td class="rowBtn inspectRowBtn">…</td>'
-    
+
 
 
     let newRow = document.createElement('tr');
@@ -21,7 +21,7 @@ function neuerAuftrag() {
     
     document.querySelector('.usertable-body').appendChild(newRow)
     console.log(newRow)
-
+    
     counter++;
 
 
@@ -46,3 +46,20 @@ function inspectRow(){
 };
 
 
+
+
+
+function showAuftragsModal() {
+    
+    let auftragsModal = document.querySelector('.neuerAuftragModal');
+    auftragsModal.style.visibility = 'visible';
+
+    let backgroundBlur = document.querySelector('.app-container');
+    backgroundBlur.style.filter = 'blur(5px)';
+}
+
+function closeAuftragsModal() {
+
+    let auftragsModal = document.querySelector('.neuerAuftragModal');
+    auftragsModal.style.visibility = 'hidden';
+}
