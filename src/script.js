92e803ef75235178a00a27e775190a511fde59fb
Gonzo <containerkid@gmail.com>
2023-01-02 09:09:15 +0100

    Minor Changes

--- src/script.js
+++ src/script.js
@@ -2,29 +2,31 @@
 let counter = 1;
 
 function neuerAuftrag() {
+    document.body.style.backgroudColor = "red";
+    // let auftragsTitle = document.querySelector('#title-input').value;
+    // console.log(auftragsTitle);
+
+
+    // let newCounterTd = `<td>${counter}</td>`;
+    // let newObjectTd = `<td>${prompt('Gib die Object Nummer ein')}</td>`;
+    // let issueTd = `<td>${auftragsTitle}</td>`;
+    // let zuweisungTd = `<td><img src="/Users/rimlight/Documents/GHV PVT APP/ghv-app/library/Memojis/IMG_4655.PNG" style="height: 30px;"></td><td>…</td><td>…</td>`;
+    // let removeBtn = '<td onclick="deleteRow()" class="rowBtn deleteRowBtn">…</td>'
+    // let inspectBtn = '<td onclick="inspectRow()" class="rowBtn inspectRowBtn">…</td>'
+    // let prioTd = '<td class="rowBtn inspectRowBtn">…</td>'
+
+
+
+    // let newRow = document.createElement('tr');
+    // newRow.classList.add('auftrags-row');
+    // newRow.innerHTML = newCounterTd + newObjectTd + issueTd + zuweisungTd + removeBtn + inspectBtn;
     
-    let auftragsTitle = document.querySelector('#auftrags-title').innerText;
-    console.log(auftragsTitle);
-    let newCounterTd = `<td>${counter}</td>`;
-    let newObjectTd = `<td>${prompt('Gib die Object Nummer ein')}</td>`;
-    let issueTd = `<td>${auftragsTitle}</td>`;
-    let zuweisungTd = `<td><img src="/Users/rimlight/Documents/GHV PVT APP/ghv-app/library/Memojis/IMG_4655.PNG" style="height: 30px;"></td><td>…</td><td>…</td>`;
-    let removeBtn = '<td onclick="deleteRow()" class="rowBtn deleteRowBtn">…</td>'
-    let inspectBtn = '<td onclick="inspectRow()" class="rowBtn inspectRowBtn">…</td>'
-    let prioTd = '<td class="rowBtn inspectRowBtn">…</td>'
-
-
-
-    let newRow = document.createElement('tr');
-    newRow.classList.add('auftrags-row');
-    newRow.innerHTML = newCounterTd + newObjectTd + issueTd + zuweisungTd + removeBtn + inspectBtn;
+    // document.querySelector('.usertable-body').appendChild(newRow)
+    // console.log(newRow)
     
-    document.querySelector('.usertable-body').appendChild(newRow)
-    console.log(newRow)
-    
-    counter++;
+    // counter++;
 
-
+    return false;
 };
 
 
