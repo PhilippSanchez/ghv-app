cdfc17bf599d3bf2cbdcd06c969a9057c159cece
Philipp Sanchez Paetzmann <containerkid@gmail.com>
2023-01-03 12:21:21 +0100

    Major Changes

--- src/script.js
+++ src/script.js
@@ -37,6 +37,65 @@ function neuerAuftrag() {
 
 
 
+function addIssue() {
+
+
+let date = new Date().toLocaleDateString("de-DE");
+
+
+
+let issueTitle = document.getElementById('title-input');
+let issueObjekt = document.getElementById('objektInput');
+
+
+
+let newRowElement = document.createElement('div');
+    newRowElement.classList.add('listItem');
+    newRowElement.innerHTML = `
+    <div class="itemSelector">
+      <input type="checkbox" name="itemSelector" id="itemSelector">
+    </div>
+    <div class="listIssueSection">
+      <a href="" class="listLink">${issueTitle.value}</a>
+      <div class="issueSubText">
+          <span class="issueLocation"> ${issueObjekt.value} dies das Annanas str.</span>
+          <span class="issueOpendBy">Andreas Sanchez Paetzmann</span>
+          <span class="lastTimeEdited">${date}</span>
+      </div>
+    </div>  
+
+    <div class="issueAssignment">
+         <img src="/Users/rimlight/Documents/GHV PVT APP/ghv-app/library/Memojis/IMG_4655.PNG">
+    </div>
+
+    <div class="prioSet">
+         <img src="/Users/rimlight/Documents/GHV PVT APP/ghv-app/library/person.circle@2x.png" alt="">
+    </div>
+
+    <div class="auftragsNummer">
+          <h4>${counter}</h4>
+    </div>
+    <div class="deleteRow"><span><img src="library/Icons/delete.left@2x.png"></span></div>
+`;
+
+document.querySelector('.auftragsListe').appendChild(newRowElement);
+
+closeAuftragsModal()
+
+counter++;
+
+issueTitle.value = '';
+issueObjekt.value = '';
+
+
+}
+
+
+function setPrio() = {
+    
+}
+
+
 function showAuftragsModal() {
     
     let auftragsModal = document.querySelector('.neuerAuftragModal');
@@ -50,14 +109,9 @@ function closeAuftragsModal() {
 
     let auftragsModal = document.querySelector('.neuerAuftragModal');
     auftragsModal.style.visibility = 'hidden';
+
+    let backgroundBlur = document.querySelector('.auftragsListe');
+    backgroundBlur.style.filter = 'blur(0px)';
 }
 
-
-function closeLoginModal() {
-
-    let loginModal = document.querySelector('#loginForm');
-
-    loginModal.style.visibility = 'hidden';
-
-}
 
